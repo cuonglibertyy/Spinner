f86e6c4900ac7ef515d20e2b0e255fe89f1f637b
cuonglibertyy <cuonglibertyy@gmail.com>
2019-06-26 09:31:59 +0700

    update

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -2,90 +2,71 @@ package com.example.spinner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.spinner.adapter.SpinnerAdapter;
-import com.example.spinner.model.Phone;
+import com.example.spinner.adapter.CountryAdapter;
+import com.example.spinner.model.CountryItem;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    AppCompatSpinner spn_Sdt;
-    public TextView tv_sdt;
-    EditText ed_Sdt;
-    Button btn_submit;
+    private ArrayList<CountryItem> mCountryList;
+    private CountryAdapter mAdapter;
+    String mavung ="";
+    String sdt = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initList();
 
+        Spinner spinnerCountries = findViewById(R.id.spinner_countries);
+        Button btn_submit = findViewById(R.id.btn_submit);
+        final EditText ed_sdt = findViewById(R.id.ed_Sdt);
+        final  TextView tv_sdt = findViewById(R.id.tv_sdt);
+        btn_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sdt = ed_sdt.getText().toString().trim();
 
+                tv_sdt.setText(mavung+sdt);
 
-        anhxa();
+            }
+        });
 
-       final ArrayList<Phone> phoneList1;
+        mAdapter = new CountryAdapter(this, mCountryList);
+        spinnerCountries.setAdapter(mAdapter);
 
-
-       phoneList1 = new ArrayList<>();
-       phoneList1.add(new Phone("#035","1"));
-        phoneList1.add(new Phone("#0398","2"));
-        phoneList1.add(new Phone("#032","3"));
-        phoneList1.add(new Phone("#031","4"));
-
-       // khởi tạo 1 list đối tượng là sđt
-        // tạo ra  1 đối tượng empty để hiển thị mặc định spinner
-
-
-        final SpinnerAdapter spinnerAdapter = new SpinnerAdapter(MainActivity.this,phoneList1);
-        spn_Sdt.setAdapter(spinnerAdapter);
-        spn_Sdt.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinnerCountries.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Phone phone = phoneList1.get(position);
-                String mavung = phone.mavung;
-
-
-                Toast.makeText(MainActivity.this,phone.mavung, Toast.LENGTH_SHORT).show();
+                CountryItem clickedItem = (CountryItem) parent.getItemAtPosition(position);
+                mavung = clickedItem.getCountryName();
+                Toast.makeText(MainActivity.this, mavung + " selected", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
             }
-
         });
 
-
-
-        btn_submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String sdt =  ed_Sdt.getText().toString().trim();
-                tv_sdt.setText(mavung+sdt);
-
-
-            }
-
-
-        });
     }
 
-    private void anhxa() {
-    spn_Sdt = findViewById(R.id.spn_Sdt);
-    tv_sdt = findViewById(R.id.tv_sdt);
-    ed_Sdt = findViewById(R.id.ed_Sdt);
-    btn_submit = findViewById(R.id.btn_submit);
-
+    private void initList() {
+        mCountryList = new ArrayList<>();
+        mCountryList.add(new CountryItem("+034", R.drawable.velezuela));
+        mCountryList.add(new CountryItem("+098", R.drawable.velezuela));
+        mCountryList.add(new CountryItem("+890", R.drawable.velezuela));
+        mCountryList.add(new CountryItem("+67", R.drawable.velezuela));
     }
 }
